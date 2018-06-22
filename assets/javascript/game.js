64525d58a656e8e18b8b89d414a66e5100cd695c
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-06-21 19:03:18 -0700

    this commit includes more CSS changes and javascript additions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //variables for the game
 
-var randomNum = "";
+var magicNum = "";
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
@@ -12,5 +12,31 @@ var white;
 var yellow;
 var crystalValue;
 
-//random number for user to match
-randomNum = Math.floor((Math.random() * 100) + 19);
+//the magic number that the user needs to match
+magicNum = Math.floor((Math.random() * 100) + 19);
+
+//numbers, wins and losses...
+$('#magicNum').text(randomNum);
+$('#userScore').text(totalScore);
+$('#wins').text('Wins: '+ wins);
+$('#losses').text('Losses: ' + losses);
+
+//create the function that assigns a number for the crystals
+function numCrystals() {
+crystals = num.sort(function(a, b){return 0.5 - Math.random()});
+
+blue = crystals[0];
+green = crystals[1];
+red = crystals[2];
+purple = crystals[3];
+
+//now it is time to assign a value to each crystal so that when clicked they put out a specific value
+$('#crystal1').attr('data-value', blue);
+$('#crystal2').attr('data-value', green);
+$('#crystal3').attr('data-value', red);
+$('#crystal4').attr('data-value', purple);
+}
+
+numCrystals();
+//values associated with each crystal...
+console.log(crystals, blue, green, red, purple);
